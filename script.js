1b3ba36d82dad50690135f3d9fe9e81e63ea0b39
susmitamenka <susmitaprasad73@gmail.com>
2025-05-12 14:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+function delay(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    document.getElementById('btn').addEventListener('click', () => {
+      const inputValue = parseFloat(document.getElementById('ip').value);
+      if (!isNaN(inputValue)) {
+        transformNumber(inputValue);
+      } else {
+        alert('Please enter a valid number');
+      }
+    });
+
+    function transformNumber(num) {
+      delay(2000)
+        .then(() => {
+          document.getElementById('output').innerText = `Result: ${num}`;
+          return num * 2;
+        })
+        .then(result => {
+          return delay(2000).then(() => {
+            document.getElementById('output').innerText = `Result: ${result}`;
+            return result - 3;
+          });
+        })
+        .then(result => {
+          return delay(1000).then(() => {
+            document.getElementById('output').innerText = `Result: ${result}`;
+            return result / 2;
+          });
+        })
+        .then(result => {
+          return delay(1000).then(() => {
+            document.getElementById('output').innerText = `Result: ${result}`;
+            return result + 10;
+          });
+        })
+        .then(finalResult => {
+          return delay(1000).then(() => {
+            document.getElementById('output').innerText = `Final Result: ${finalResult}`;
+          });
+        });
+    }
